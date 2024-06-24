73ddf0d634f6204fe4edaea9bcaac7d9af657133
Andrew Lebedev <166292267+4ndew@users.noreply.github.com>
2024-06-24 15:45:11 +0700

    Update browser.py

--- core/browser.py
+++ core/browser.py
@@ -1,1 +1,14 @@
+from PyQt6.QtCore import QUrl
 
+class Browser:
+    def __init__(self, web_view):
+        self.web_view = web_view
+
+    def load_url(self, url):
+        self.web_view.setUrl(QUrl(url))
+
+    def get_current_url(self):
+        return self.web_view.url().toString()
+
+    def run_javascript(self, script, callback):
+        self.web_view.page().runJavaScript(script, callback)
