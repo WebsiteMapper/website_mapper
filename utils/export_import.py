9bcab8eed5b761776ad151f897bee3412407aa56
Andrew Lebedev <166292267+4ndew@users.noreply.github.com>
2024-06-24 15:45:52 +0700

    Update export_import.py

--- utils/export_import.py
+++ utils/export_import.py
@@ -1,1 +1,12 @@
+import json
+import networkx as nx
 
+def export_map(graph, file_name):
+    data = nx.node_link_data(graph)
+    with open(file_name, 'w') as f:
+        json.dump(data, f)
+
+def import_map(file_name):
+    with open(file_name, 'r') as f:
+        data = json.load(f)
+    return nx.node_link_graph(data)
